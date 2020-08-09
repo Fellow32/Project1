b64fe8a1ca1cab3e9eece6718686d2789d765de5
Konstantin <65258089+Fellow32@users.noreply.github.com>
2020-08-09 15:19:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(function () {
+//   $(".header_slider").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     asNavFor: ".header_nav",
+//   });
+
+//   $(".header_nav").slick({
+//     arrows: false,
+//     slidesToShow: 5,
+//     asNavFor: ".header_slider",
+//     focusOnSelect: true,
+//   });
+// });
+
+
